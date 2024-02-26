05dfb26e37fdb231ad65644be2788baef45c9c8b
ahmedammarsd <ahmed091999287@gmail.com>
2024-02-26 09:56:30 +0200

    all

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,29 @@
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  const { userId } = auth();
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        creditBalance={user.creditBalance}
+        type={transformation.type as TransformationTypeKey}
+      />
+    </>
+  );
 };
 
 export default AddTransformationTypePage;
--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,12 +1,11 @@
 import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URL = process.env.MONGODG_URL;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
-
 
 let cached: MongooseConnection = (global as any).mongoose;
 
@@ -15,6 +14,7 @@ if (!cached) {
 }
 
 export const connectToDatabase = async () => {
+  //console.log(MONGODB_URL)
   if (cached.conn) return cached.conn; //1- check if already have a cached connection
 
   if (!MONGODB_URL) throw new Error("MONGODB_URL is not defined");
@@ -25,7 +25,7 @@ export const connectToDatabase = async () => {
     mongoose.connect(MONGODB_URL, {
       dbName: "imaginify",
       bufferCommands: false,
-    });
-     cached.conn = await cached.promise;
-     return cached.conn;
+    }); 
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
